242fbeb025b4c8d2f4788862096578a9ac26f039
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-05 04:17:37 +0100

    [+] function to display window, stucked at 800x800

---
+++ src/setupwindow.py
@@ -0,0 +1,19 @@
+import pygame
+from displayboard import display_board
+
+def main():
+
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption("SaltyyChess, First python project")
+    display_board(screen)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        pygame.display.flip()
+    pygame.quit()
+if __name__ == "__main__":
+    main()
