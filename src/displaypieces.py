268b849907b444c812b1fa58ad9b1dff78b1b98b
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-05 18:45:03 +0100

    [~] pieces are now displayed, however there is still an issue in the size, need to check it may come from the png

--- src/displaypieces.py
+++ src/displaypieces.py
@@ -13,14 +13,26 @@ pieces_pos = [
     ("knight_white", (6, 7)), ("rook_white", (7, 7))
 ]
 
-def load_draw_pieces(screen, pieces_path="assets/pieces/"):
-
+def load_draw_pieces(screen):
     SIZE = 100
+    x_ajust = 10
+    new_size = (80, 80)
+
+    current_file_path = os.path.abspath(__file__)
+    current_dir = os.path.dirname(current_file_path)
+    pieces_path = os.path.join(current_dir, '..', 'assets', 'pieces')
 
     for piece_name, position in pieces_pos:
         image_path = os.path.join(pieces_path, piece_name + ".png")
-        piece_image = pygame.image.load(image_path)
-        pixel_pos = (position[0] * SIZE, position[1] * SIZE)
-        screen.blit(piece_image, pixel_pos)
-
+        try:
+            piece_image = pygame.image.load(image_path).convert_alpha()
+            if "knight" in piece_name or "bishop" in piece_name or "pawn" in piece_name:
+                piece_image = pygame.transform.scale(piece_image, new_size)
+                pixel_pos = ((position[0] * SIZE) + (SIZE - new_size[0]) // 2 + x_ajust,
+                             (position[1] * SIZE) + (SIZE - new_size[1]) // 2)
+            else:
+                pixel_pos = (position[0] * SIZE + x_ajust, position[1] * SIZE)
+            screen.blit(piece_image, pixel_pos)
+        except pygame.error as e:
+            print(f"Erreur lors du chargement de l'image {image_path}: {e}")
 
