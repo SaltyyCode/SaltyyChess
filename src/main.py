41ffce611b25d0d613d4f45c293972685fa8acbf
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-07 02:36:49 +0100

    [~] added boardpieces, drawboard and drawpieces function to draw the chess board and the pieces :)

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import pygame as p
 import chess_board
-from draw_board_pieces import *
+
+
 WIDTH = HEIGHT = 800
 DIMENSION = 8
 SQUARE_SIZE = WIDTH // DIMENSION
@@ -11,7 +12,26 @@ def loadpng():
 
     pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("assets/pieces" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+        IMAGES[piece] = p.transform.scale(p.image.load("assets/pieces/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
+
+def boardpieces(screen, gs):
+    drawboard(screen)
+    drawpieces(screen, gs.board)
+
+
+def drawboard(screen):
+    colors = [p.Color(255, 255, 255), p.Color(98, 62, 8)]
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            color = colors[((x+y) % 2)]
+            p.draw.rect(screen, color, p.Rect(y*SQUARE_SIZE, x*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+def drawpieces(screen, board):
+    for x in range(DIMENSION):
+        for y in range(DIMENSION):
+            piece = board[x][y]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(y*SQUARE_SIZE, x*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
 
 def main():
@@ -22,16 +42,14 @@ def main():
     clock = p.time.Clock()
     screen.fill((255, 255, 255))
     gs = chess_board.GameState()
-    drawboard(screen)
-    #loadpng()
-    print(gs.board)
+    loadpng()
     running = True
 
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-        
+        boardpieces(screen, gs)
         clock.tick(FPS_MAX)
         p.display.flip()
 
