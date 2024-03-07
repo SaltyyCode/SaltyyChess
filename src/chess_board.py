903f42d4d41becc4797fcbec726284621f510302
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-07 15:27:46 +0100

    [~] added a new class to handle piece move

--- src/chess_board.py
+++ src/chess_board.py
@@ -12,4 +12,14 @@ class GameState():
             ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"],]
         self.whiteMove = True
         self.moveLog = []
-        
+
+class Move():
+
+    def __init__(self, stSquare, endSquare, board):
+        self.startRow = stSquare[0]
+        self.startCol = stSquare[1]
+        self.endRow = endSquare[0]
+        self.endCol = endSquare[1]
+        self.movedPiece = board[self.startRow][self.startCol]
+        self.capturedPiece = board[self.endRow][self.endCol]
+         
