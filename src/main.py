3e8d375fe2592d7e3f06420e757ee5ac851c5f86
Clément // Saltyy <clementbouret59@gmail.com>
2024-03-08 21:32:33 +0100

    [~] added some function to color the square where each piece can go if clicked on

--- src/main.py
+++ src/main.py
@@ -14,9 +14,10 @@ def loadpng():
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("assets/pieces/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
-def boardpieces(screen, gs):
+def boardpieces(screen, gs, selectedsquare, getallMoves):
     drawboard(screen)
     drawpieces(screen, gs.board)
+    seeMoves(screen, gs, selectedsquare, getallMoves)
 
 
 def drawboard(screen):
@@ -32,6 +33,20 @@ def drawpieces(screen, board):
             piece = board[x][y]
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(y*SQUARE_SIZE, x*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+
+def seeMoves(screen, gs, selectedsquare, getallMoves):
+
+    if selectedsquare != ():
+        r, c = selectedsquare
+        if gs.board[r][c][0] == ('w' if gs.whiteMove else 'b'):
+            s = p.Surface((SQUARE_SIZE, SQUARE_SIZE))
+            s.set_alpha(100)
+            s.fill(p.Color('green'))
+            screen.blit(s, (c*SQUARE_SIZE, r*SQUARE_SIZE))
+            s.fill(p.Color('red'))
+            for move in getallMoves:
+                if move.startRow == r and move.startCol == c:
+                    screen.blit(s, (move.endCol * SQUARE_SIZE, move.endRow * SQUARE_SIZE))
 
 
 def main():
@@ -83,7 +98,7 @@ def main():
             validMove = gs.getvalidMoves()
             moveMade = False
 
-        boardpieces(screen, gs)
+        boardpieces(screen, gs, selectedsquare, validMove)
         clock.tick(FPS_MAX)
         p.display.flip()
 
