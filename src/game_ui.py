8b5adc5cb909f0cda34c1aaa42eb24dc07565b8b
Clément // Saltyy <clementbouret59@gmail.com>
2024-10-29 00:16:39 +0100

    feat: showing castle move

--- src/game_ui.py
+++ src/game_ui.py
@@ -1,47 +1,48 @@
 import pygame as p
 
-WIDTH = HEIGHT = 800
 DIMENSION = 8
-SQUARE_SIZE = WIDTH // DIMENSION
-FPS_MAX = 15
+SQUARE_SIZE = 800 // DIMENSION
 IMAGES = {}
 
-def loadpng():
+def load_images():
 
-    pieces = ['wp', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    pieces = ["bRook", "bN", "bBishop", "bQueen", "bKing", "bPawn", "wRook", "wN", "wBishop", "wQueen", "wKing", "wPawn"]
     for piece in pieces:
         IMAGES[piece] = p.transform.scale(p.image.load("assets/pieces/" + piece + ".png"), (SQUARE_SIZE, SQUARE_SIZE))
 
-def boardpieces(screen, gs, selectedsquare, getallMoves):
-    drawboard(screen)
-    drawpieces(screen, gs.board)
-    seeMoves(screen, gs, selectedsquare, getallMoves)
+def draw_game_state(screen, gs, valid_moves, selected_square):
 
+    draw_board(screen)
+    draw_pieces(screen, gs.board)
+    highlight_moves(screen, gs, selected_square, valid_moves)
 
-def drawboard(screen):
+def draw_board(screen):
+
     colors = [p.Color(255, 255, 255), p.Color(98, 62, 8)]
-    for x in range(DIMENSION):
-        for y in range(DIMENSION):
-            color = colors[((x+y) % 2)]
-            p.draw.rect(screen, color, p.Rect(y*SQUARE_SIZE, x*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            color = colors[((row + col) % 2)]
+            p.draw.rect(screen, color, p.Rect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-def drawpieces(screen, board):
-    for x in range(DIMENSION):
-        for y in range(DIMENSION):
-            piece = board[x][y]
+def draw_pieces(screen, board):
+
+    for row in range(DIMENSION):
+        for col in range(DIMENSION):
+            piece = board[row][col]
             if piece != "--":
-                screen.blit(IMAGES[piece], p.Rect(y*SQUARE_SIZE, x*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+                screen.blit(IMAGES[piece], p.Rect(col * SQUARE_SIZE, row * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-def seeMoves(screen, gs, selectedsquare, getallMoves):
+def highlight_moves(screen, gs, selected_square, valid_moves):
 
-    if selectedsquare != ():
-        r, c = selectedsquare
-        if gs.board[r][c][0] == ('w' if gs.whiteMove else 'b'):
+    if isinstance(selected_square, tuple) and len(selected_square) == 2:
+        row, col = selected_square
+
+        if gs.board[row][col][0] == ('w' if gs.white_to_move else 'b'):
             s = p.Surface((SQUARE_SIZE, SQUARE_SIZE))
             s.set_alpha(100)
             s.fill(p.Color('green'))
-            screen.blit(s, (c*SQUARE_SIZE, r*SQUARE_SIZE))
+            screen.blit(s, (col * SQUARE_SIZE, row * SQUARE_SIZE))
             s.fill(p.Color('red'))
-            for move in getallMoves:
-                if move.startRow == r and move.startCol == c:
-                    screen.blit(s, (move.endCol * SQUARE_SIZE, move.endRow * SQUARE_SIZE))
+            for move in valid_moves:
+                if move.start_row == row and move.start_col == col:
+                    screen.blit(s, (move.end_col * SQUARE_SIZE, move.end_row * SQUARE_SIZE))
