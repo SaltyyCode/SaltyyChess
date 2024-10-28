4ca3addd8530af1296716bbedfa7791cbe92d37c
Clément // Saltyy <clementbouret59@gmail.com>
2024-10-29 00:17:22 +0100

    feat: so fucking back

--- src/main.py
+++ src/main.py
@@ -1,80 +1,79 @@
 import pygame as p
 import chess_board
-from load_sounds import load_sounds
-from game_ui import loadpng, boardpieces, drawpieces, drawboard, seeMoves
-
+import game_ui
+from load_sounds import load_sounds, play_sound
 
 WIDTH = HEIGHT = 800
-DIMENSION = 8
-SQUARE_SIZE = WIDTH // DIMENSION
 FPS_MAX = 15
-IMAGES = {}
-
 
 def main():
-    
+
     p.init()
     screen = p.display.set_mode((WIDTH, HEIGHT))
-    p.display.set_caption("SaltyyChess, First python project")
     clock = p.time.Clock()
-    screen.fill((255, 255, 255))
+    screen.fill(p.Color("grey"))
     gs = chess_board.GameState()
-    validMove = gs.getvalidMoves()
-    moveMade = False
-    loadpng()
+    valid_moves = gs.get_valid_moves()
+    move_made = False
+    game_ui.load_images()
     sounds = load_sounds()
-    move_sound = sounds["move"]
-    capture_sound = sounds["capture"]
-    check_sound = sounds["check"]
-    mate_sound = sounds["mate"]
     running = True
-    selectedsquare = ()
-    playerClick = []
+    selected_square = ()
+    player_clicks = []
 
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-            elif e.type == p.MOUSEBUTTONDOWN and not gs.gameOver:
-                location = p.mouse.get_pos()
-                col = location[0]//SQUARE_SIZE
-                row = location[1]//SQUARE_SIZE
-                if selectedsquare == (row, col):
-                    selectedsquare = ()
-                    playerClick = []
+            elif e.type == p.MOUSEBUTTONDOWN:
+                loc = p.mouse.get_pos()
+                col = loc[0] // game_ui.SQUARE_SIZE
+                row = loc[1] // game_ui.SQUARE_SIZE
+                if selected_square == (row, col):
+                    selected_square = ()
+                    player_clicks = []
                 else:
-                    selectedsquare = (row, col)
-                    playerClick.append(selectedsquare)
-                if len(playerClick) == 2:
-                    move = chess_board.Move(playerClick[0], playerClick[1], gs.board)
-                    if move in validMove:
-                        gs.makeMove(move, capture_sound)
-                        if gs.IsCheck():
-                            check_sound.play()
-                        else:
-                            move_sound.play()
-                        moveMade = True
-                        selectedsquare = ()
-                        playerClick = []
-                    else: # If player clicks twice on board or on different pieces, the last click is saved.
-                        playerClick = [selectedsquare] # If a move is possible in the next click, it will be done.
-                    
-            elif e.type == p.KEYDOWN:
-                if e.key == p.K_LEFT:
-                    gs.undo()
-                elif e.key == p.K_RIGHT:
-                    gs.redo()
-        
-        if moveMade:
-            validMove = gs.getvalidMoves()
-            moveMade = False
-            if gs.CheckMate:
-                mate_sound.play()
-            if gs.CheckGameStatus() == True:
-                validMove = []
-                
+                    selected_square = (row, col)
+                    player_clicks.append(selected_square)
+                if len(player_clicks) == 2:
+                    move = chess_board.Move(player_clicks[0], player_clicks[1], gs.board)
+                    for i in range(len(valid_moves)):
+                        if move == valid_moves[i]:
+                            gs.make_move(valid_moves[i])
+                            move_made = True
+                            selected_square = ()
+                            player_clicks = []
+                            if move.piece_captured != "--":
+                                play_sound(sounds, "capture")
+                            else:
+                                play_sound(sounds, "move")
 
-        boardpieces(screen, gs, selectedsquare, validMove)
+                    if not move_made:
+                        player_clicks = [selected_square]
+
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_z:
+                    gs.undo_move()
+                    move_made = True
+
+            if gs.checkmate:
+                if gs.white_to_move:
+                    print("Black wins")
+                else:
+                    print("White wins")
+                play_sound(sounds, "mate")
+                running = False
+            elif gs.stalemate:
+                print("Stalemate")
+                running = False
+            elif gs.in_check():
+                play_sound(sounds, "check")  # Jouer le son d'échec
+
+        if move_made:
+            valid_moves = gs.get_valid_moves()
+            move_made = False
+
+        game_ui.draw_game_state(screen, gs, valid_moves, selected_square)
         clock.tick(FPS_MAX)
         p.display.flip()
 
