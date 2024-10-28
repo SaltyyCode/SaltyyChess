e1dc4937ab69f4a8e23a0ced19c789a8ed94b8f5
Clément // Saltyy <clementbouret59@gmail.com>
2024-10-29 00:17:01 +0100

    feat: new sound

--- src/load_sounds.py
+++ src/load_sounds.py
@@ -1,11 +1,15 @@
 import pygame as p
 
 def load_sounds():
-
+    p.mixer.init()
     sounds = {
-        "move": p.mixer.Sound("assets/sounds/move.mp3"),
-        "capture": p.mixer.Sound("assets/sounds/capture.mp3"),
-        "check": p.mixer.Sound("assets/sounds/check.mp3"),
-        "mate": p.mixer.Sound("assets/sounds/mat.mp3"),
+        'move': p.mixer.Sound("assets/sounds/move.mp3"),
+        'capture': p.mixer.Sound("assets/sounds/capture.mp3"),
+        'check': p.mixer.Sound("assets/sounds/check.mp3"),
+        'mate': p.mixer.Sound("assets/sounds/mat.mp3")
     }
     return sounds
+
+def play_sound(sounds, action):
+    if action in sounds:
+        sounds[action].play()
